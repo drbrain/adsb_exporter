a98c0d8c541094adff6b69ecb032bc795a59be95
Eric Hodel <drbrain@segment7.net>
2021-12-15 18:09:42 -0800

    Add aircraft observations

--- src/aircraft_json.rs
+++ src/aircraft_json.rs
@@ -3,18 +3,27 @@ use crate::fetch::fetch;
 use anyhow::Context;
 use anyhow::Result;
 
+use geo::algorithm::bearing::Bearing;
+use geo::prelude::*;
+use geo::Coordinate;
+use geo::Line;
+use geo::Point;
+
 use lazy_static::lazy_static;
 
 use log::debug;
 
 use prometheus::register_gauge_vec;
+use prometheus::register_int_gauge_vec;
 use prometheus::GaugeVec;
+use prometheus::IntGaugeVec;
 
 use reqwest::Client;
 
 use serde_json::json;
 use serde_json::Value;
 
+use std::collections::HashMap;
 use std::time::Duration;
 
 use tokio::time::sleep;
@@ -36,6 +45,12 @@ lazy_static! {
         "adsb_aircraft_recent_mlat_total",
         "Number of aircraft recently observed with a position determined by multilateration",
         &[&"frequency"],
+    )
+    .unwrap();
+    static ref OBSERVATIONS: IntGaugeVec = register_int_gauge_vec!(
+        "adsb_aircraft_observations",
+        "Number of aircraft positions observed by range and bearing",
+        &[&"frequency", &"bearing", &"distance"],
     )
     .unwrap();
 }
@@ -125,6 +140,54 @@ impl AircraftJson {
             })
             .count();
 
+        let receiver_position = Coordinate {
+            x: 47.59,
+            y: -122.30,
+        };
+        let receiver_point: Point<f64> = receiver_position.into();
+
+        let mut observations = HashMap::with_capacity(positions);
+
+        aircrafts
+            .iter()
+            .filter(|a| {
+                if let Some(seen) = a.get("seen_pos") {
+                    seen.as_f64().unwrap_or(f64::INFINITY) < 60.0
+                } else {
+                    false
+                }
+            })
+            .for_each(|a| {
+                let aircraft_lat = a.get("lat").unwrap().as_f64().unwrap();
+                let aircraft_lon = a.get("lon").unwrap().as_f64().unwrap();
+
+                let aircraft_position = Coordinate {
+                    x: aircraft_lat,
+                    y: aircraft_lon,
+                };
+
+                let distance = Line::new(aircraft_position, receiver_position).haversine_length();
+                let distance_bucket = (1 + (distance / 80_000.0) as u32) * 80_000;
+                let distance_bucket = match distance_bucket {
+                    0..=400_000 => distance_bucket.to_string(),
+                    _ => "> 400000".to_string(),
+                };
+
+                let bearing = receiver_point.bearing(aircraft_position.into());
+                // The documentation says "North is 0° and East is 90°" but this doesn't seem to
+                // match the results, so we need to rotate by 90, then add positive bearing.
+                let bearing = (450.0 + bearing) % 360.0;
+
+                let bearing_bucket = (((bearing + 11.25) / 22.5).floor() * 22.5) % 360.0;
+                let bearing_bucket = bearing_bucket.to_string();
+
+                let key = (distance_bucket, bearing_bucket);
+                let previous = observations.get(&key).unwrap_or(&0);
+                let count = *previous + 1;
+
+                observations.insert(key, count);
+            });
+
         RECENT_OBSERVED
             .with_label_values(&[&self.frequency])
             .set(observed as f64);
@@ -134,11 +197,13 @@ impl AircraftJson {
         RECENT_MLAT
             .with_label_values(&[&self.frequency])
             .set(mlat as f64);
-
-        debug!(
-            "aircraft observed: {}, position: {} mlat: {}",
-            observed, positions, mlat
-        );
+        observations
+            .iter()
+            .for_each(|((distance, bearing), count)| {
+                OBSERVATIONS
+                    .with_label_values(&[&self.frequency, &bearing, &distance])
+                    .set(*count)
+            });
 
         Ok(())
     }
