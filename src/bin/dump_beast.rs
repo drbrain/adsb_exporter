f805e6446d37f47f2d1721ed97ded5ea7b8d3d45
Eric Hodel <drbrain@segment7.net>
2022-01-15 20:20:27 -0800

    Use clap in dump_beast

--- src/bin/dump_beast.rs
+++ src/bin/dump_beast.rs
@@ -2,11 +2,22 @@ use anyhow::Result;
 
 use adsb_exporter::beast::Codec;
 
+use clap::Parser;
+
 use futures_util::StreamExt;
 
 use tokio::fs::File;
 
 use tokio_util::codec::Framed;
+
+/// Dump messages from a BEAST server
+#[derive(Parser)]
+#[clap(about, version)]
+struct Args {
+    /// Process a file containing BEAST data
+    #[clap(long)]
+    pub file: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -14,8 +25,9 @@ async fn main() -> Result<()> {
 
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
-    let file = std::env::args().nth(1).unwrap();
-    let stream = File::open(file).await?;
+    let args = Args::parse();
+
+    let stream = File::open(args.file).await?;
 
     let mut reader = Framed::new(stream, Codec::new());
 
